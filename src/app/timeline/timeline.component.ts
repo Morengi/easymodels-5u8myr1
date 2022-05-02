d72ef3cb4a1d111285e2c33e2ae32d8f2ac7cf1e
Morengi <lykyanov.sergey@rambler.ru>
2022-05-02 15:56:07 +0300

    2 files modified

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
-
-
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
   styleUrls: ['./timeline.component.css'],
 })
+export class TimeLineComponent implements OnInit {
+  ngOnInit() {}
 
+  timeLine_Nodes: timeNode[] = [];
 
-export class TimeLineComponent implements OnInit{
+  addTimeNode(timeNode, card) {
+    timeNode.timeLine_Nodes.push({
+      user: 'придумаем',
+      card_name: card.name,
+      date: card.date,
+    });
 
-  
-  ngOnInit(){}
-  
-  
+    console.log(timeNode.user, timeNode.card_name, timeNode.date);
   }
+}
+
+type timeNode = {
+  user: string;
+  card_name: string;
+  date: number;
+};
